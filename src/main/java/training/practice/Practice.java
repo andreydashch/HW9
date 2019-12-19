b8c8e1ae2a40142c0e404681dd609111b087d5a7
andreydashch <andreydashch@gmail.com>
2019-12-19 22:44:47 +0200

    The easiest way

--- src/main/java/training/practice/Practice.java
+++ src/main/java/training/practice/Practice.java
@@ -1,7 +1,24 @@
 package training.practice;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.TreeMap;
+
 public class Practice {
     public static void main(String[] args) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        NotReduceArrayList<Integer> notReduceArrayList = new NotReduceArrayList<>();
 
+        notReduceArrayList.addAll(Arrays.asList(4, 5, -6, 4, 5, 3, 4, 2, 4, 5, 7));
+
+        notReduceArrayList.forEach(x -> {
+                    if (map.containsKey(x)) {
+                        map.put(x, map.get(x) + 1);
+                    } else {
+                        map.put(x, 1);
+        }});
+
+        System.out.println(map);
     }
 }
